7db666a504e131a41d783cc50352f00ce0a90651
Eduardo Mena Ciudad <emena@hiberus.local>
2019-02-26 09:54:56 +0100

    Added entities: Transaction and Person

---
+++ DivisasBackofficeAPI/Entities/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivisasBackofficeAPI.Entities
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage = "Name length minimum is 3 characters")]
+        [MaxLength(50, ErrorMessage = "Name length maximum is 50 characters")]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "Surname length minimum is 2 characters")]
+        [MaxLength(50, ErrorMessage = "Surname length maximum is 50 characters")]
+        public string Surname { get; set; }
+        [MaxLength(11, ErrorMessage = "Phone maximum length = 11 digits")]
+        public string Phone { get; set; }
+    }
+}
---
+++ DivisasBackofficeAPI/Entities/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivisasBackofficeAPI.Entities
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Concept { get; set; }
+        [Required]
+        [Range(0 , double.MaxValue, ErrorMessage = "Negative prices are not allowed")]
+        public float Price { get; set; }
+        [Required]
+        public Currency Unit { get; set; }
+
+        public Person Client{ get; set; }
+    }
+}
