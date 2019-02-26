cca305bb6d051bb157397ccce09964d323ba2838
Eduardo Mena Ciudad <emena@hiberus.local>
2019-02-26 09:44:28 +0100

    Initial Commit

---
+++ DivisasBackofficeAPI/Entities/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DivisasBackofficeAPI.Entities
+{
+    public class Currency
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(5,  ErrorMessage = "Currency name can't be shorter than 5 characters")]
+        [MaxLength(50, ErrorMessage = "Currency name can't be longer than 50 characters")]
+        public string Title { get; set; }
+        [Required]
+        public char Simbol { get; set; }
+        [MaxLength(1024, ErrorMessage = "Icon size can't be bigger than 1 MB")]
+        public byte[] Icon { get; set; }
+    }
+}
---
+++ DivisasBackofficeAPI/Entities/Exchange.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivisasBackofficeAPI.Entities
+{
+    public class Exchange
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public Currency Origin { get; set; }
+        [Required]
+        public Currency Destiny { get; set; }
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Exchange value can't be negative")]
+        public float Value { get; set; }
+    }
+}
