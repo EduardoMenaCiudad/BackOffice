83ac141c5a0639039a0567cb7fadac8615aa5d35
Eduardo Mena Ciudad <emena@hiberus.local>
2019-02-26 10:54:15 +0100

    Database updated, modification in migration cs

---
+++ DivisasBackofficeAPI/DatabaseContext/DivisasContext.cs
@@ -0,0 +1,23 @@
+﻿using DivisasBackofficeAPI.Entities;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivisasBackofficeAPI.DatabaseContext
+{
+    public class DivisasContext : DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<Exchange>  Exchanges{ get; set; }
+        public DbSet<Person> People { get; set; }
+
+
+        public DivisasContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ DivisasBackofficeAPI/Migrations/20190226090348_InitialMigration.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DivisasBackofficeAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(maxLength: 50, nullable: false),
+                    Simbol = table.Column<string>(nullable: false),
+                    Icon = table.Column<byte[]>(maxLength: 1024, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(maxLength: 50, nullable: false),
+                    Phone = table.Column<string>(maxLength: 11, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exchanges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OriginId = table.Column<int>(nullable: false),
+                    DestinyId = table.Column<int>(nullable: false),
+                    Value = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exchanges", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Exchanges_Currencies_DestinyId",
+                        column: x => x.DestinyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Exchanges_Currencies_OriginId",
+                        column: x => x.OriginId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Concept = table.Column<string>(nullable: false),
+                    Price = table.Column<float>(nullable: false),
+                    UnitId = table.Column<int>(nullable: false),
+                    ClientId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transactions_People_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Currencies_UnitId",
+                        column: x => x.UnitId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exchanges_DestinyId",
+                table: "Exchanges",
+                column: "DestinyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exchanges_OriginId",
+                table: "Exchanges",
+                column: "OriginId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_ClientId",
+                table: "Transactions",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_UnitId",
+                table: "Transactions",
+                column: "UnitId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exchanges");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+        }
+    }
+}
