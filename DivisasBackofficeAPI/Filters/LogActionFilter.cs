97a02622b7cc723d1d8a32811e0223576a7f40af
Eduardo Mena Ciudad <emena@hiberus.local>
2019-02-27 09:46:06 +0100

    Added Filters folder, with LogActionFilter and a sample AsyncActionFilter

---
+++ DivisasBackofficeAPI/Filters/LogActionFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivisasBackofficeAPI.Filters
+{
+    public class LogActionFilter : IActionFilter
+    {
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine($" ///// Request = {context.HttpContext.Request}");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine($" ///// Response = {context.HttpContext.Response}");
+        }
+    }
+}
---
+++ DivisasBackofficeAPI/Filters/MySampleAsyncActionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivisasBackofficeAPI.Filters
+{
+    public class MySampleAsyncActionFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            Console.WriteLine(context.ModelState.IsValid);
+
+            var resultContext = await next();
+        }
+    }
+}
